249c14ffacf99c63838c833f29e19058dc8b9bca
kahsin <ksyang1121@gmail.com>
2023-10-14 18:51:25 +0800

    change drivers table type to users

--- database/migrations/2023_10_14_103440_create_orders_table.php
+++ database/migrations/2023_10_14_103440_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('driver_id');
-            $table->foreign('driver_id')->references('id')->on('drivers');
+            $table->foreign('driver_id')->references('id')->on('users');
             $table->unsignedBigInteger('customer_id');
             $table->foreign('customer_id')->references('id')->on('customers');
             $table->unsignedFloat('total_prices');
