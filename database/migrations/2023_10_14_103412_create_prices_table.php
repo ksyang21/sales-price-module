375bcb577fb300ab7276e35a5b8d1a6406713765
kahsin <ksyang1121@gmail.com>
2023-10-14 18:47:08 +0800

    change price to unsigned

--- database/migrations/2023_10_14_103412_create_prices_table.php
+++ database/migrations/2023_10_14_103412_create_prices_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreign('product_id')->references('id')->on('products');
             $table->unsignedBigInteger('customer_id');
             $table->foreign('customer_id')->references('id')->on('customers');
-            $table->float('price');
+            $table->unsignedFloat('price');
             $table->unsignedBigInteger('max_stock')->comment('maximum count for this customer of this price, if exceed limit, then use original price');
             $table->timestamps();
         });
