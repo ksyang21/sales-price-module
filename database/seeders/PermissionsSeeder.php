be068c9ad130ff3f2ca4f0a0ce6c6fb252e9abfd
kahsin <ksyang1121@gmail.com>
2023-10-14 16:31:36 +0800

    seed permissions

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name'=>'create user']);
+        Permission::create(['name'=>'edit user']);
+        Permission::create(['name'=>'delete user']);
+        Permission::create(['name'=>'set price']);
+        Permission::create(['name'=>'view customer']);
+
+        $admin_role = Role::create(['name' => 'admin']);
+        $admin_role->givePermissionTo('create user');
+        $admin_role->givePermissionTo('edit user');
+        $admin_role->givePermissionTo('delete user');
+        $admin_role->givePermissionTo('set price');
+        $admin_role->givePermissionTo('view customer');
+
+        $driver_role = Role::create(['name' => 'driver']);
+        $driver_role->givePermissionTo('view customer');
+
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@interview.com',
+        ]);
+        $admin->assignRole($admin_role);
+
+        $drivers = User::factory()->count(5)->create();
+        foreach($drivers as $driver) {
+            $driver->assignRole($driver_role);
+        }
+    }
+
+}
