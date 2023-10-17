5fab4b92c3f43853c7f65d11cb1e9cf51afab8e9
ksyang21 <ksyang1121@hotmail.com>
2023-10-17 23:55:08 +0800

    add driver customer data

---
+++ database/migrations/2023_10_17_153106_create_driver_customers.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('driver_customers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('driver_id');
+            $table->foreign('driver_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers')->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('driver_customers');
+    }
+};
