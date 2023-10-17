2798343125a7215467e474035cd5d6895ca4b623
ksyang21 <ksyang1121@hotmail.com>
2023-10-17 23:55:23 +0800

    new seeding data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Customer;
+use App\Models\DriverCustomer;
 use App\Models\Order;
 use App\Models\OrderDetails;
 use App\Models\Price;
@@ -18,74 +19,82 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Customer::factory(10)->create();
-        for($i = 1; $i <31; $i++) {
-            $product = sprintf('Item %d', $i);
+        Customer::factory(30)->create();
+        for ($i = 1; $i < 31; $i++) {
+            $product      = sprintf('Item %d', $i);
             $product_data = [
-                'name' => $product,
+                'name'  => $product,
                 'price' => fake()->randomFloat(2, 0, 3000),
             ];
             Product::create($product_data);
         }
 
         $customers = Customer::all();
-        $products = Product::all();
+        $products  = Product::all();
         foreach ($customers as $customer) {
             foreach ($products as $product) {
                 $original_price = $product->price;
-                $price_data = [
-                    'product_id' => $product->id,
+                $price_data     = [
+                    'product_id'  => $product->id,
                     'customer_id' => $customer->id,
-                    'price' => mt_rand(0, $original_price * 100) / 100.0,
-                    'max_stock' => rand(1, 10),
+                    'price'       => mt_rand(0, $original_price * 100) / 100.0,
+                    'max_stock'   => rand(1, 10),
                 ];
                 Price::create($price_data);
             }
         }
 
-        for ($i = 0; $i < 10; $i++) {
-            $driver = User::where('id', '>', 1)->inRandomOrder()->first();
-            $customer = Customer::inRandomOrder()->first();
-            $status = fake()->randomElement(['pending', 'completed', 'cancelled']);
-            $order = Order::create([
-                'driver_id' => $driver->id,
-                'customer_id' => $customer->id,
-                'status' => $status,
-            ]);
+        $drivers = User::where('id', '>', 1)->get();
+        foreach ($drivers as $driver) {
+            for ($i = ($driver->id - 2) * 5 + 1; $i <= ($driver->id - 1) * 5; $i++) {
+                $customer             = Customer::find($i);
+                $driver_customer_data = [
+                    'driver_id'   => $driver->id,
+                    'customer_id' => $customer->id,
+                ];
+                DriverCustomer::create($driver_customer_data);
 
-            $selected_products = [];
-            for ($details = 0; $details < rand(1, 5); $details++) {
-                do {
-                    $product = Product::inRandomOrder()->first();
-                } while(in_array($product, $selected_products));
-                $selected_products[] = $product;
+                $status = fake()->randomElement(['pending', 'completed', 'cancelled']);
+                $order  = Order::create([
+                    'driver_id'   => $driver->id,
+                    'customer_id' => $customer->id,
+                    'status'      => $status,
+                ]);
 
-                $price = Price::where('product_id', $product->id)->where('customer_id', $customer->id)->first();
-                $purchase_quantity = rand(1, 15);
-                if ($purchase_quantity < $price->max_stock) {
-                    $order_details = [
-                        'order_id' => $order->id,
-                        'product_id' => $product->id,
-                        'price' => $price->price,
-                        'quantity' => $purchase_quantity
-                    ];
-                } else {
-                    $order_details = [
-                        'order_id' => $order->id,
-                        'product_id' => $product->id,
-                        'price' => $price->price,
-                        'quantity' => $price->max_stock
-                    ];
+                $selected_products = [];
+                for ($details = 0; $details < rand(1, 5); $details++) {
+                    do {
+                        $product = Product::inRandomOrder()->first();
+                    } while (in_array($product, $selected_products));
+                    $selected_products[] = $product;
+
+                    $price             = Price::where('product_id', $product->id)->where('customer_id', $customer->id)->first();
+                    $purchase_quantity = rand(1, 15);
+                    if ($purchase_quantity < $price->max_stock) {
+                        $order_details = [
+                            'order_id'   => $order->id,
+                            'product_id' => $product->id,
+                            'price'      => $price->price,
+                            'quantity'   => $purchase_quantity,
+                        ];
+                    } else {
+                        $order_details = [
+                            'order_id'   => $order->id,
+                            'product_id' => $product->id,
+                            'price'      => $price->price,
+                            'quantity'   => $price->max_stock,
+                        ];
+                        OrderDetails::create($order_details);
+
+                        $order_details = [
+                            'order_id'   => $order->id,
+                            'product_id' => $product->id,
+                            'price'      => $product->price,
+                            'quantity'   => ($purchase_quantity - $price->max_stock),
+                        ];
+                    }
                     OrderDetails::create($order_details);
-
-                    $order_details = [
-                        'order_id' => $order->id,
-                        'product_id' => $product->id,
-                        'price' => $product->price,
-                        'quantity' => ($purchase_quantity - $price->max_stock)
-                    ];
                 }
-                OrderDetails::create($order_details);
             }
         }
     }
