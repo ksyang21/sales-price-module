32097c7ae77079a19d17853e6e3e6428ee193114
ksyang21 <ksyang1121@hotmail.com>
2023-10-19 23:05:28 +0800

    update admin and new driver account

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -33,14 +33,19 @@ class PermissionsSeeder extends Seeder
 
         $admin = User::factory()->create([
             'name' => 'Admin',
-            'email' => 'admin@interview.com',
+            'email' => 'admin@example.com',
         ]);
         $admin->assignRole($admin_role);
 
-        $drivers = User::factory()->count(5)->create();
+        $drivers = User::factory()->count(4)->create();
         foreach($drivers as $driver) {
             $driver->assignRole($driver_role);
         }
+        $driver = User::factory()->create([
+            'name' => 'Driver 1',
+            'email' => 'driver@example.com',
+        ]);
+        $driver->assignRole($driver_role);
     }
 
 }
