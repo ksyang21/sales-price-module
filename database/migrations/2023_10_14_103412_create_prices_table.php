632669e7da0348e27307f1c7e7aca1464a2ac54f
ksyang21 <ksyang1121@hotmail.com>
2023-10-22 16:22:58 +0800

    fix enum bug price table

--- database/migrations/2023_10_14_103412_create_prices_table.php
+++ database/migrations/2023_10_14_103412_create_prices_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete();
             $table->unsignedBigInteger('customer_id');
             $table->foreign('customer_id')->references('id')->on('customers')->cascadeOnDelete();
-            $table->enum('type', ['special price module', 'foc module'])->default('special_price');
+            $table->enum('type', ['special price module', 'foc module'])->default('special price module');
             $table->unsignedFloat('price');
             $table->unsignedBigInteger('max_stock')->comment('maximum count for this customer of this price, if exceed limit, then use original price');
             $table->unsignedInteger('foc_quantity');
